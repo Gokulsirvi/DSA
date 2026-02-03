0c5fc1de8f1753a4d9e671b32d27a56d16bd4f31
Gokul sirvi <gokulsirvi6@gmail.com>
2026-02-03 08:40:28 +0530

    Bubble Sort optimal approach
    Refactor Bubble Sort algorithm to improve efficiency by adding a flag to track if any swaps were made during the iteration. This allows the algorithm to terminate early if the array is already sorted.

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -8,13 +8,19 @@ public class BubbleSort{
   }
   static void sort(int[] arr){
     int n = arr.length;
+    int flag = 0;
     for(int i=0;i<n;i++){
-      for(int j = i+1;j<n;j++){
-        if(arr[i]>arr[j]){
-          int temp = arr[i];
-          arr[i] = arr[j];
-          arr[j] = temp;
+      flag = 0;
+      for(int j=0;j<n-i-1;j++){
+        if(arr[j]>arr[j+1]){
+          flag =1;
+          int temp = arr[j];
+          arr[j] = arr[j+1];
+          arr[j+1] = temp;
         }
+      }
+      if(flag==0){
+        break;
       }
     }
   }
