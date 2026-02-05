0654bf65c9d6c8607417e9969bd84e04d930d903
Gokul sirvi <gokulsirvi6@gmail.com>
2026-02-05 08:35:14 +0530

    Update InsertionSort.java

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -1,20 +1,23 @@
 import java.util.*;
-public class InsertionSort{
-public static void main(string[] args){
+public class Main{
+public static void main(String[] args){
   int[] arr = {2,3,4,5,2,1};
   sort(arr);
   System.out.println(Arrays.toString(arr));
 }
   static void sort(int[] arr){
     int n = arr.length;
-    for(int i=0;i<n;i++){
-      int j = i;
-      while(j>0 && arr[j]>arr[i]){
-        j--;
+    for(int i=1;i<n;i++){
+      //select a key = arr[i]
+        int key = arr[i];
+      //take a pointer 
+      int j = i-1;
+      //run a loop and find the correct position of key and swap another to +1 index
+      while(j>=0 && arr[j]>key){
+       arr[j+1] = arr[j];
+       j=j-1;
       }
-      int temp = arr[i];
-      arr[i] = arr[j];
-      arr[j] = temp;
+      arr[j+1] = key;
     }
   }
 }
