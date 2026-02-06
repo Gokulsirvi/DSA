d6d956de74970537637c723471dab4a980b1f27a
Gokul sirvi <gokulsirvi6@gmail.com>
2026-02-06 09:05:16 +0530

    Fix quickSort implementation and adjust parameters

--- Sorting/QuickSort.java
+++ Sorting/QuickSort.java
@@ -2,20 +2,38 @@ import java.util.*;
 public class Main{
 public static void main(String[] args){
   int[] arr = {7,3,4,5,2,1};
-  quickSort(arr,0,arr.length);
+  quickSort(arr,0,arr.length-1);
   System.out.println(Arrays.toString(arr));
 }
-  // implementation of Quick sort algorithm via recursion
-  static void quickSort(int[] arr,int left,int right){
-    int pivot = left+(right-left)/2;
-    while(left > right){
-      while(arr[left] > arr[pivot]) left++;
-      while(arr[right] < arr[pivot]) right--;
-      int temp = arr[left];
-      arr[left] = arr[right];
-      arr[right] = temp;
+ // implementation of Quick sort algo via recursion
+
+static void quickSort(int[] arr, int left, int right) {
+    // Base Case: Stop if the subarray has 0 or 1 element
+    if (left >= right) return;
+
+    int i = left;
+    int j = right;
+    int pivot = arr[left + (right - left) / 2]; 
+
+    while (i <= j) {
+        // Find element on left that should be on right
+        while (arr[i] < pivot) i++;
+        // Find element on right that should be on left
+        while (arr[j] > pivot) j--;
+
+        if (i <= j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
     }
-    quickSort(arr,left,pivot);
-    quickSort(arr,right,pivot);
-  }
+
+    // Recursion call 
+    if (left < j) quickSort(arr, left, j);
+    if (i < right) quickSort(arr, i, right);
+}
+
+
 }
